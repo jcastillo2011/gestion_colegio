e33a6950f9ca56eb1f57dbc1474592bf570f5b04
AngelRomero <asebastianbrito@gmail.com>
2025-10-26 19:06:46 -0400

    pages en proceso

--- django_base/settings.py
+++ django_base/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'model_students'
+    'model_students',
+    'pages'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login settings
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/dashboard/'
---
+++ pages/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+    return render(request, 'registration/login.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home') 
