5a95cf140748dbdccbfcb6fcd679ad4820aef36e
AngelRomero <asebastianbrito@gmail.com>
2025-10-26 16:35:31 -0400

    modelos para admin

--- model_students/admin.py
+++ model_students/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 from .models import Student, Teacher, Course, Evaluation
 # Register your models here.
 
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Course)
-admin.site.register(Evaluation)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name_student', 'ci', 'email')
+    search_fields = ('name_student', 'email', 'ci')
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name_teacher', 'ci', 'email')
+    search_fields = ('name_teacher', 'email', 'ci')
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name_course', 'teacher')
+    search_fields = ('name_course', 'teacher__name_teacher')
+
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('date', 'subject', 'type', 'score', 'course', 'student')
+    search_fields = ('subject', 'type', 'course__name_course', 'student__name_student')
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Evaluation, EvaluationAdmin)
--- model_students/migrations/0001_initial.py
+++ model_students/migrations/0001_initial.py
@@ -49,13 +49,13 @@ class Migration(migrations.Migration):
                 ('subject', models.CharField(max_length=40)),
                 ('type', models.CharField(max_length=20)),
                 ('score', models.DecimalField(decimal_places=2, max_digits=5)),
-                ('course', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='model_estudiantes.course')),
-                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='model_estudiantes.student')),
+                ('course', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='model_students.course')),
+                ('student', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='model_students.student')),
             ],
         ),
         migrations.AddField(
             model_name='course',
             name='teacher',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='model_estudiantes.teacher', verbose_name='Profesor Asignado'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='model_students.teacher', verbose_name='Profesor Asignado'),
         ),
     ]
