14d6158cfd36e3b92e4137e10376f27e197d3e54
AngelRomero <asebastianbrito@gmail.com>
2025-11-01 17:14:30 -0400

    Salón de clase finalizado

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('classroom/', views.classroom, name='classroom'),
     path('update-evaluations/', views.update_evaluations, name='update_evaluations'),
     path('logout/', views.logout_view, name='logout'),
 ]
